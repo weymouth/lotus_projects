db0b8103ead7083a9d7a7d7ca8f0126022e9fadb
gabe <gabe@8a68d6c3-8a5d-0410-83ce-fb90cdcd2048>
2011-06-20 02:23:46 +0000

    modified input and pre/post files for new trunk. increased resolution at bow
    git-svn-id: http://svn.mindache.net/xNFA/projects@272 8a68d6c3-8a5d-0410-83ce-fb90cdcd2048

--- bigRed/design_geom.f90
+++ bigRed/design_geom.f90
@@ -3,12 +3,13 @@
 !-------------------------------------------------------!
 program design_geom
   use analytic
+  use transform
   implicit none
   open(7,file='inp.geom')
 !
 ! -- write pointer to the IGS,ISG2 files and make srf,grd files
   write(7,'(a4)') 'body'
-  call set_write(7,.set.(/'inp.IGS ','inp.IGS2'/))
+  call set_write(7,.set.'bigRed.IGS'.map.((init_affn()**(/30.,0.,0./))+(/0.25,0.,0.065/)))
 !
 ! -- free interface set
   write(7,'(a4)') 'fint'
