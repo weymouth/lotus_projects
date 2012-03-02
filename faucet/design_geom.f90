dcc956bf029a3720f18e0ed6abb9344ecd8d2a6e
gabe <gabe@8a68d6c3-8a5d-0410-83ce-fb90cdcd2048>
2012-03-02 03:20:01 +0000

    updated geom and input file
    updated geom using the new "dots" version of the screen to mix up the jet
    
    updated inp.txt to match new input format
    
    git-svn-id: http://svn.mindache.net/xNFA/projects@313 8a68d6c3-8a5d-0410-83ce-fb90cdcd2048

--- faucet/design_geom.f90
+++ faucet/design_geom.f90
@@ -18,12 +18,15 @@ program design_geom
   write(7,'(a4)') 'body'
   pipe = .set.cylinder(1,ndims,r0,0,0,velo)-.set.plane(1,norm,tip,0,velo) &
      .or.(.set.cylinder(1,ndims,r0*1.1,0,0,0)-.set.cylinder(1,ndims,r0,0,0,0)-.set.plane(1,norm,tip,0,0))
+  pipe = pipe.or.(.set.screen(1,ndims,tip,r0*0.355).and..set.cylinder(1,ndims,r0*1.1,0,0,0))
   call set_write(7,pipe)
-
+!
 ! -- geom_fint
   write(7,'(a4)') 'fint'
-  water = .set.cylinder(1,ndims,r0,0,0,0)-.set.plane(1,norm,tip,0,velo) &
-       .or..set.plane(1,norm,0,0,0)
+  water = .set.cylinder(1,ndims,r0,0,0,0).or..set.plane(1,norm,0,0,0)
   call set_write(7,water)
-
+!
+! -- velo of water in pipe
+  write(7,'(a4)') 'velo'
+  call set_write(7,pipe)
 end program design_geom
