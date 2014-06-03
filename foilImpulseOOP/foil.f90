a7a66645b1b78bdbc43b307591d37c6923c0f6f9
Gabriel Weymouth <drweymouth@google.com>
2014-06-03 10:17:11 +0800

    Updated foil and viz files. Running in 2D and 3D

--- foilImpulseOOP/foil.f90
+++ foilImpulseOOP/foil.f90
@@ -2,29 +2,28 @@
 !--------------- Foil Impulse Test case ----------------!
 !-------------------------------------------------------!
 program foil_impulse
-  use mympiMod
-  use fluidMod
-  use vectorMod
-  use bodyMod
-  use geom_shape ! to define the geomtry
+  use fluidMod,   only: fluid
+  use bodyMod,    only: body
+  use mympiMod,   only: init_mympi,mympi_end,mympi_rank
+  use geom_shape  ! to create geometry
   implicit none
-  integer,parameter  :: f=2**4             ! resolution  
-  real,parameter     :: Re = 410           ! Reynolds number
-  integer,parameter  :: ndims=3            ! dimensions
-  integer,parameter  :: b(3) = (/4,1,4/)   ! blocks
-  integer,parameter  :: d(3) = (/4,4,4/) ! domain size
+  integer,parameter  :: f=2**6             ! resolution  
+  real,parameter     :: Re = 1000          ! Reynolds number
+  real(8),parameter  :: alpha = 10         ! AOA
+  integer,parameter  :: b(3) = (/2,4,2/)   ! blocks
+  integer,parameter  :: d(3) = (/6,4,6/)   ! domain size
 !
+  integer,parameter  :: ndims = 3   ! dimensions
   real(8),parameter  :: L = f       ! length
   integer,parameter  :: m(3) = f*d  ! points
   real,parameter     :: nu = L/Re   ! viscosity
   real(8),parameter  :: yc = m(2)/2 ! location
-  real(8),parameter  :: zc = m(3)/2 ! location
-  real(8),parameter  :: alpha = 10  ! AOA
   integer            :: n(3)
-  real               :: force(3)
+  real               :: force(3),area
 !
   type(fluid)        :: flow
   type(body)         :: foil
+  type(set)          :: geom
   type(model_info)   :: info
 !
 ! -- Initialize MPI (if MPI is ON)
@@ -37,33 +36,35 @@ program foil_impulse
   n = m
 #endif
   if(ndims==2) n(3) = 1
+  if(mympi_rank()==0) print *, '-- Foil Impulse --'
+  if(mympi_rank()==0) print '("   L=",i0," nu=",f0.4)', f,nu
 !
 ! -- Initialize the foil geometry
-  surface_debug = .true.
+!  surface_debug = .true.
   model_fill = .false.
   info%file = 'naca_square.IGS'
   info%x = (/-4.219,-10.271,0./)
   info%s = 0.36626*L
-  foil = (model_init(info) &
-       .map.((init_affn()**(/alpha,0.D0,0.D0/))+(/zc,yc,0.D0/)))&
-       .and.plane(4,1,(/0,0,-1/),(/zc,yc,zc/),0,0)
+  geom = (model_init(info) &
+       .map.((init_affn()**(/alpha,0.D0,0.D0/))+(/yc,yc,0.D0/)))
+  if(ndims==3) geom = geom.and.plane(4,1,(/0,0,-1/),yc,0,0)
+  foil = geom
+  area = L
+  if(ndims==3) area = L*(m(3)-yc)
 !
 ! -- Initialize fluid
   call flow%init(n,foil,V=(/1.,0.,0./),nu=nu)
   call flow%write
-  stop
-  if(mympi_rank()==0) print *, '-- Foil Impulse --'
-  if(mympi_rank()==0) print '("   L=",i0," nu=",f0.4)', f,nu
+  if(mympi_rank()==0) print *, '-- init complete --'
 !
 ! -- Run it
-  do while (flow%time<100*L)
+  do while (flow%time<L)
      call flow%update
-     if(mod(flow%time,5*L)<flow%dt) call flow%write
+     if(mod(flow%time,0.05*L)<flow%dt) call flow%write
 !
 ! -- Print force on the square
      force = foil%pforce(flow%pressure)
-     if(ndims==3) force = force/real(m(3))
-     write(9,'(f10.4,f7.4,3e16.8)') flow%time/L,flow%dt,2.*force/L
+     write(9,'(f10.4,f7.4,3e16.8)') flow%time/L,flow%dt,2.*force/area
      flush(9)
   end do
   if(mympi_rank()==0) write(6,*) '--- complete --- '
