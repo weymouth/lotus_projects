b7d6a42f603dfff2f6fed3176d1e7a5a925912f4
Gabriel Weymouth <drweymouth@google.com>
2014-07-30 12:28:36 +0100

    Make using oop/Makefile

--- perch/lotus.f90
+++ perch/lotus.f90
@@ -12,7 +12,7 @@ program perch
   real,parameter     :: Re = 2e3           ! Reynolds number
   real,parameter     :: Xi = 0.125         ! Shape change number
   integer,parameter  :: b(3) = (/4,4,1/)   ! blocks
-  integer,parameter  :: d(3) = (/8,8,1/)   ! domain size
+  integer,parameter  :: d(3) = (/6,6,1/)   ! domain size
 !
   integer,parameter  :: ndims = 2   ! dimensions
   real(8),parameter  :: L = f       ! length
@@ -21,7 +21,7 @@ program perch
   real,parameter     :: T = L/Xi    ! motion period
   integer            :: n(3)
   real               :: t0,t1,dt,dtPrint=0.1
-  real               :: area,u,u0,force(3)
+  real               :: area,u,a,force(3)
 !
   type(fluid)        :: flow
   type(body)         :: foil
@@ -37,7 +37,10 @@ program perch
   n = m
 #endif
   if(ndims==2) n(3) = 1
-  xg(1:2)%left = -m(1:2)
+  xg(1:2)%left = -m(1:2)/2.
+  if(mympi_rank()==0) call xg(1)%write
+  if(mympi_rank()==0) call xg(2)%write
+  if(mympi_rank()==0) call xg(3)%write
 !
 ! -- Init
   if(mympi_rank()==0) print *, '-- Perch Test --'
@@ -45,7 +48,7 @@ program perch
 !
 ! -- Initialize the foil geometry
   info%file = 'input.IGS'
-  info%x = (/-4.219,-10.271,-18.876/)
+  info%x = (/-4.219,-10.271,-16.876/)
   info%s = 0.36626*L*(/1,1,-1/)
   foil = model_init(info).map.jerk(5,6,0,T,0,0)
   area = L
@@ -54,29 +57,30 @@ program perch
 ! -- Initialize fluid
   call flow%init(n,foil,nu=nu)
   call flow%resume
-  if(flow%time==0.) flow%time = -5*L
+!!$  if(flow%time==0.) flow%time = -5*L
   if(mympi_rank()==0) print *, '-- init complete --'
 !
 ! -- Time update loop
-  do while (flow%time/T<2)
+  do while (flow%time/T<0.2)
      dt = flow%dt/T
      t0 = flow%time/T
      t1 = t0+dt
 !
 ! -- Deccelerate
-     u = 1; u0 = 1
-     if(t0>0) u0 = 1-t0
-     if(t1>0) u  = 1-t1
-     if(t0>1) u0 = 0
-     if(t1>1) u  = 0
+     u = t1
+     if(t1>1) u = 1.
      flow%velocity%e(1)%bound_val = u
-     flow%g(1) = (u-u0)/flow%dt
-     if(mympi_rank()==0) print 1,t1,flow%g(1),u
+
+     a = (u-t0)/flow%dt
+     if(t0>1) a = 0
+     flow%g(1) = a
+
+     if(mympi_rank()==0) print 1,t1,a,u
 1    format("   t=",f0.4," g=",f0.4," u=",f0.4)
 !
 !-- update and write fluid
      call flow%update
-     if(mod(t1,dtPrint)<dt) call flow%write
+     if(mod(abs(t1),dtPrint)<dt) call flow%write
 !
 ! -- print force
      force = foil%pforce(flow%pressure)
