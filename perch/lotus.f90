0f7f5f816bb3334f9c1c63ea8bf5d2492775dd8c
Gabriel Weymouth <drweymouth@google.com>
2014-08-07 16:40:59 +0100

    Updated and test. Good agreement except for 5% reduced lift

--- perch/lotus.f90
+++ perch/lotus.f90
@@ -5,14 +5,14 @@ program perch
   use fluidMod,   only: fluid
   use bodyMod,    only: body
   use mympiMod,   only: init_mympi,mympi_end,mympi_rank
-  use geom_shape  ! to create geometry
   use gridMod,    only: xg
+  use geom_shape  ! to define geom (set,eps,plane, etc)
   implicit none
   integer,parameter  :: f=3*2**5           ! resolution  
   real,parameter     :: Re = 2e3           ! Reynolds number
-  real,parameter     :: Xi = 0.125         ! Shape change number
+  real,parameter     :: Xi = 0.5           ! Shape change number
   integer,parameter  :: b(3) = (/4,4,1/)   ! blocks
-  integer,parameter  :: d(3) = (/6,6,1/)   ! domain size
+  integer,parameter  :: d(3) = (/4,4,1/)   ! domain size
 !
   integer,parameter  :: ndims = 2   ! dimensions
   real,parameter     :: L = f       ! length
@@ -25,7 +25,6 @@ program perch
 !
   type(fluid)        :: flow
   type(body)         :: foil
-  type(model_info)   :: info
 !
 ! -- Initialize MPI (if MPI is ON)
 #if MPION
@@ -38,6 +37,8 @@ program perch
 #endif
   if(ndims==2) n(3) = 1
   xg(1:2)%left = -m(1:2)/2.
+  call xg(1)%init(m(1),0.75*L,1.25*L,1.0,r=1.018)
+  call xg(2)%init(m(2),0.25*L,1.75*L,1.0,r=1.018)
   if(mympi_rank()==0) call xg(1)%write
   if(mympi_rank()==0) call xg(2)%write
   if(mympi_rank()==0) call xg(3)%write
@@ -47,17 +48,18 @@ program perch
   if(mympi_rank()==0) print '("   L=",i0," nu=",f0.4)', f,nu
 !
 ! -- Initialize the foil geometry
-  foil = naca(L,0.12,(/0.16,0.,0./))
+  foil = naca(L,0.15).map.init_rigid(6,alpha,omega)
   area = L
   if(ndims==3) area = L*m(3)
 !
-! -- Initialize fluid
+  ! -- Initialize fluid
   call flow%init(n,foil,V=(/1.,0.,0./),nu=nu)
+
   flow%velocity%e%exit = .true.
-  call flow%resume
-  if(flow%time==0.) flow%time = -5*L
+!  if(ndims==3) call flow%velocity%e(1)%perturb(0.05)
+  if(flow%time==0.) flow%time = -L*5
   if(mympi_rank()==0) print *, '-- init complete --'
-!
+  !
 ! -- Time update loop
   do while (flow%time/T<2)
      dt = flow%dt/T
@@ -65,21 +67,22 @@ program perch
      t1 = t0+dt
 !
 ! -- Deccelerate
-     u = 1.; u0 = 1
-     if(t0>0) u0 = 1.-t0
-     if(t0>1) u0 = 0.
-     if(t1>0) u = 1.-t1
-     if(t1>1) u = 0.
-     flow%velocity%e(1)%bound_val = u
+     u  = min(1.,max(0.,1.-t1))
+     u0 = min(1.,max(0.,1.-t0))
      a = (u-u0)/flow%dt
+     flow%velocity%e(1)%bound_val = u
      flow%g(1) = a
-
-     if(mympi_rank()==0) print 1,t1,a,u
-1    format("   t=",f0.4," g=",f0.4," u=",f0.4)
+!
+! -- rotate
+     if(t1>0 .and. t0<1) then
+        call foil%update(t1)
+     end if
+     if(mympi_rank()==0) print 1,t1,a,u,alpha(DBLE(t1)),omega(DBLE(t1))
+1    format("   t=",f0.4," g=",f0.4," u=",f0.4," alpha=",f0.4," omega=",f0.4)
 !
 !-- update and write fluid
-     call flow%update
-     if(mod(abs(t1),dtPrint)<dt) call flow%write
+     call flow%update(foil)
+     if(mod(t1,dtPrint)<dt .and. t1>0) call flow%write(foil)
 !
 ! -- print force
      force = foil%pforce(flow%pressure)
@@ -87,44 +90,43 @@ program perch
      flush(9)
   end do
   if(mympi_rank()==0) write(6,*) '--- complete --- '
-!
+  !
 ! -- Finalize MPI
 #if MPION
   call mympi_end
 #endif
 
 contains
-  type(set) function naca(c,t,o)
-    real,intent(in)   :: c,t,o(3)
-    integer :: m = 20,i
-    real    :: p(3)=0,q(3)=0,n(3)
-
-    p = 0; 
-    q(1) = (1./m)**2; q(2) = t*oord(q(1))
-    print *,q
-    n = (/p(2)-q(2),q(1)-p(1),0./)
-    n = n/sqrt(sum(n*n))
-    naca = plane(4,1,n,c*(p-o),0,0).and.plane(4,1,mirror(n),mirror(c*(p-o)),0,0)
-
-    do i=2,m
-       p = q
-       q(1) = (real(i)/m)**2; q(2) = t*oord(q(1))
-       print *,q
-       n = (/p(2)-q(2),q(1)-p(1),0./)
-       n = n/sqrt(sum(n*n)) 
-       naca = naca.and.plane(4,1,n,c*(p-o),0,0).and.plane(4,1,mirror(n),mirror(c*(p-o)),0,0)
-    end do
-
+!
+! -- geometry definitions
+  type(set) function naca(c,o)
+    real,intent(in) :: c,o
+    type(model_info) :: info
+    type(affn) :: shift
+    info%file = 'naca_square.IGS'
+    info%x = (/-4.219,-10.271,-18.876/)
+    info%s = 0.36626*c*(/1,1,-1/)
+    info%n = (/c,c,1./)
+    shift = init_affn()+DBLE(c*(/0.5-o,0.,0./)) ! shift pivot to origin
+    surface_debug = .true.
+    eps = 2.0
+    naca = model_init(info).map.shift
   end function naca
-
-  function mirror(a) result(b)
-    real,intent(in) :: a(3)
-    real :: b(3)
-    b = a; b(2) = -b(2)
-  end function mirror
-
-  real function oord(x) result(y)
-    real,intent(in) :: x
-    y = 5*(0.2969*sqrt(x)-0.1260*x-0.3516*x**2+0.2843*x**3-0.1015*x**4);
-  end function oord
+!
+! -- motion definitions
+  real(8) pure function alpha(ts)  ! rotation angle
+    real(8),intent(in) :: ts
+    if(ts>1) then
+       alpha = pi/2.
+    else if(ts>0) then
+       alpha = pi/2.*(ts-sin(2.*pi*ts)/(2.*pi))
+    end if
+  end function alpha
+  real(8) pure function omega(ts)  ! rotation velocity
+    real(8),intent(in) :: ts
+    omega = 0
+    if(ts>0 .and. ts<1) then
+       omega = pi/2.*(1-cos(2.*pi*ts))/T
+    end if
+  end function omega
 end program perch
