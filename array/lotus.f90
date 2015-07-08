9d0f8309c87774ecd76701bf5466b8b5afd8073f
Gabriel Weymouth <drweymouth@google.com>
2015-07-08 13:06:27 +0100

    Committing array changes

--- array/lotus.f90
+++ array/lotus.f90
@@ -9,38 +9,49 @@ program array
   use geom_shape
   implicit none
 
-  integer,parameter :: rows = 4, finish = 1021, dtPrint = 100
-  real,parameter    :: D = 16, DG = 21*D, Re_D = 100, nu = D/Re_D
-  integer,parameter :: b(3) = (/4,4,1/)
+  integer,parameter :: rows = 2, ndims = 3, finish = 600
+  real,parameter    :: D = 16, DG = 7*D, Re_D = 100, nu = D/Re_D
+  integer,parameter :: b(3) = (/4,4,1/), dtPrint = 10
+  logical,parameter :: p(3) = (/.false.,.false.,.true./), single=.false.
 
   type(fluid) :: flow
   type(body)  :: bodies
   integer     :: n(3)
-  real        :: dt, t1, pforce(2), vforce(2)
+  real        :: dt, t1, pforce(ndims), vforce(ndims), A
   logical     :: root
 !
 ! -- Initialize simulation
-  call init_mympi(2,set_blocks=b(:2))
+  call init_mympi(ndims,set_blocks=b(:ndims),set_periodic=p(:ndims))
   root = mympi_rank()==0
 
-  n = composite(DG*(/4,2,0/), root)
-  call xg(1)%stretch(n(1), -10*DG, -0.6*DG, 0.6*DG, 25*DG, h_max=15., prnt=root)
+  if(ndims==3) then
+     n = composite((/6.0*DG,2.4*DG,16*D/), root)
+     xg(3)%h = 0.5*DG/D
+  else
+     n = composite((/6.0*DG,2.4*DG,0./), root)
+  end if
+  call xg(1)%stretch(n(1), -10*DG, -0.6*DG, 0.6*DG, 20*DG, h_max=DG/D, prnt=root)
   call xg(2)%stretch(n(2), -10*DG, -0.6*DG, 0.6*DG, 10*DG, prnt=root)
 
-  bodies = make_array()
+  if(single) then
+     bodies = cylinder(1,1,3,0.5*DG,center=0.)
+  else
+     bodies = make_array()
+  end if
+
+  A = DG*n(3)*xg(3)%h
   call flow%init(n/b, bodies, nu=nu)
-  if(flow%time==0.) call flow%write(bodies)
 
   if(root) print *, '-- init complete --'
 !
 ! -- Time update loop
-  do while (flow%time/D<finish)
+  do while (flow%time/DG<finish)
      dt = flow%dt
      t1 = flow%time+dt
 !
 ! -- smoothly increase flow speed
-     flow%velocity%e(1)%bound_val = tanh(t1/(50*D))
-     flow%g(1) = sech(t1/(50*D))**2/(50*D)
+     flow%velocity%e(1)%bound_val = tanh(t1/(50*DG))
+     flow%g(1) = sech(t1/(50*DG))**2/(50*DG)
 !
 ! -- update flow
      call flow%update()
@@ -48,15 +59,14 @@ program array
 ! -- print force coefficients
      pforce = -bodies%pforce(flow%pressure)
      vforce = nu*bodies%vforce(flow%velocity)
-     write(9,'(f10.4,f8.4,4e16.8)') t1/D,dt,2.*pforce/DG,2.*vforce/DG
+     write(9,'(f10.4,f8.4,4e16.8)') t1/DG,dt,2.*pforce(:2)/A,2.*vforce(:2)/A
      flush(9)
 !
 ! -- print flow
-     if(mod(t1,dtPrint*D)<dt) call flow%write()
+     if(mod(t1,dtPrint*DG)<dt) call flow%write()
   end do
 
   call mympi_end
-
 contains
 !
 ! -- hyperbolic secant that is stable for large x
@@ -67,7 +77,8 @@ contains
 !
 ! -- make an array of cylinders with group diameter DG
   type(set) function make_array()
-    integer,parameter :: n(4) = (/6,13,19,25/)
+!    integer,parameter :: n(4) = (/6,13,19,25/)
+    integer,parameter :: n(4) = (/6,12,18,26/) ! edit for symmetry
     real,parameter    :: R = 0.5*DG-0.5*D
     real    :: theta,xc,yc
     integer :: i,j
