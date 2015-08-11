939e7989c09f953d54be12de50932c78adc368c4
Gabriel Weymouth <drweymouth@google.com>
2015-08-11 15:19:53 +0800

    Added bouyant force to compare plot

--- sphere/compare.R
+++ sphere/compare.R
@@ -12,6 +12,10 @@ B = read.table("GustTp2D100/fort.9",col.names = c("time","CFL","drag","lift","Fz
 B$name = "gust"
 
 data = rbind(A,B)
+data$bouy = 0
+data$bouy[data$time<=2.2 & data$name=="gust"] = 2.5*4/3
 
-drag = qplot(time,4*drag,data=data,linetype=name,geom='line')+labs(y=expression(C[Dp]),x='tU/D',linetype="")
+drag = qplot(time,-4*drag,data=data,linetype=name,geom='line')+
+  geom_line(aes(y=bouy),data=subset(data,bouy>0))+
+  labs(y=expression(C[Dp]),x='tU/D',linetype="")
 ppdf(drag,"drag_compare.pdf")
