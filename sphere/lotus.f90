8a3ce93030ec1f4eedcf98e1d6376faaa6686880
Gabriel Weymouth <drweymouth@google.com>
2017-06-13 10:29:30 +0100

    Improve sphere lotus.f90

--- sphere/lotus.f90
+++ sphere/lotus.f90
@@ -1,36 +1,46 @@
 program sphere_flow
-  use mympiMod,   only: init_mympi,mympi_rank,mympi_end
-  use gridMod,    only: xg
-  use geom_shape, only: sphere
   use bodyMod,    only: body
   use fluidMod,   only: fluid
+  use mympiMod,   only: init_mympi,mympi_end,mympi_rank
+  use gridMod,    only: xg
+  use geom_shape, only: sphere,pi
   use imageMod,   only: display
   implicit none
 !
 ! -- Define parameter, declare variables
-  real,parameter :: D = 2*32, Re = 30e3, dprnt = 0.5, lim = 20./D
-  integer :: n(3) = 32*(/4,2,2/), b(3) = (/4,2,2/)
-  logical :: there = .false.
+  real,parameter :: D = 2*32, Re = 30e3 ! diameter and Reynolds number
+  integer :: n(3) = 32*(/4,2,2/)  ! numer of points
+  integer :: b(3) = (/4,2,2/)     ! MPI blocks (product must equal n_procs)
+  logical :: root                 ! root processor
   type(fluid) :: flow
   type(body) :: geom
-  call init_mympi(ndims=3,set_blocks=b)
 !
-! -- Initialize grid, body and fluid
-  call xg(1)%stretch(n(1), -2*D, -0.5*D, 0.5*D, 5*D, h_min=2., h_max=12.)
-  call xg(2)%stretch(n(2), -2*D, -0.5*D, 0.5*D, 2*D, h_min=2.)
-  call xg(3)%stretch(n(3), -2*D, -0.5*D, 0.5*D, 2*D, h_min=2.)
+! -- Initialize MPI (if MPI is OFF, b is set to 1)
+  call init_mympi(3,set_blocks=b)
+  root = mympi_rank()==0
+!
+  if(root) print *,'Setting up the grid, body and fluid'
+  if(root) print *,'-----------------------------------'
+  call xg(1)%stretch(n(1), -2*D, -0.5*D, 0.5*D, 5*D, h_min=2., h_max=12.,prnt=root)
+  call xg(2)%stretch(n(2), -2*D, -0.5*D, 0.5*D, 2*D, h_min=2.,prnt=root)
+  call xg(3)%stretch(n(3), -2*D, -0.5*D, 0.5*D, 2*D, h_min=2.,prnt=root)
   geom = sphere(2, 1, radius=0.5*D, center=0)
   call flow%init(n/b,geom,V=(/1.,0.,0./),nu=D/Re)
 !
-! -- Time update loop
-  do while(.not.there)
+  if(root) print *,'Starting time update loop'
+  if(root) print *,'-----------------------------------'
+  if(root) print *,' -t- , -dt- '
+  do while(flow%time<10*D)
     call flow%update
-    if(mod(flow%time,D*dprnt)<flow%dt) then
-      if(mympi_rank()==0) print '(f6.1,",",f6.3)',flow%time/D,flow%dt
-      call display(flow%velocity%vorticity_Z(), '01vort', lim = lim)
+    write(9,'(f10.4,f8.4,3e16.8)') flow%time/D,flow%dt, &
+          2.*geom%pforce(flow%pressure)/(pi*D**2/4.)
+    if(mod(flow%time,0.1*D)<flow%dt) then
+      if(root) print '(f6.1,",",f6.3)',flow%time/D,flow%dt
+      call display(flow%velocity%vorticity_Z(), 'out_vort', lim = 20./D)
     end if
-    inquire(file='.kill', exist=there)
   end do
+  if(root) print *,'Loop complete: writing restart files and exiting'
+  if(root) print *,'-----------------------------------'
   call flow%write()
-  call mympi_end()
+  call mympi_end
 end program sphere_flow
