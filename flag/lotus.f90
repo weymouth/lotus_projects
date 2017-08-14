ffd0d974b78f918786ce2017884b3069a7b5007b
Gabriel Weymouth <drweymouth@google.com>
2017-08-14 15:35:18 +0100

    Add flexible flag project

---
+++ flag/lotus.f90
@@ -0,0 +1,183 @@
+program fish
+  use bodyMod,    only: body
+  use fluidMod,   only: fluid
+  use mympiMod,   only: init_mympi,mympi_end,mympi_rank
+  use gridMod,    only: xg,composite
+  use imageMod,   only: display
+  use geom_shape
+  implicit none
+!
+! -- Physical parameters
+  real,parameter     :: Re = 5e3, f = 1, m_star = 1E-2, lam = 1
+!
+! -- Numerical parameters
+  real,parameter     :: c = 128, m(3) = (/2.,1.6,0./), h_min = 2
+  integer,parameter  :: s = c/h_min
+  integer            :: b(3) = (/4,4,1/), box(4) = (/-0.5*c,-0.4*c,3*c,0.8*c/)
+!
+! -- resultant parameters
+  real,parameter     :: mu = m_star*c, mu_a = lam*c, k=2*pi/(lam*c)
+  real,parameter     :: omega = 2*pi*f/c, EI=(mu+mu_a)*omega**2/k**4
+!
+! -- Variables
+  real :: y(s)=0,y0(s)=0,y00(s)=0,doty(s)=0,ddoty(s)=0
+  integer :: n(3)
+  logical :: root, there = .false., flowing = .true.
+  type(fluid) :: flow
+  type(body) :: geom
+!
+! -- Initialize
+  print *,'Kb=',EI/c**3
+
+  call init_mympi(2,set_blocks=b)
+  root = mympi_rank()==0
+  if(root) print *,'Setting up the grid, body and fluid'
+  if(root) print *,'-----------------------------------'
+  n = composite(c*m,prnt=root)
+  call xg(1)%stretch(n(1), -6*c, -0.2*c, 1.2*c, 7*c, h_min=h_min, h_max=16.,prnt=root)
+  call xg(2)%stretch(n(2), -2.4*c, -0.2*c, 0.2*c, 2.4*c, prnt=root)
+  geom = rect().map.init_warp(2,h,doth,dh)
+  call flow%init(n/b,geom,V=(/1.,0.,0./),nu=c/Re,exit=.true.)
+  flow%dt = 0.5
+  call display(flow%velocity%vorticity_Z(), 'out_vort', lim = 0.25, box=box)
+!
+  if(root) print *,'Starting time update loop'
+  if(root) print *,'-----------------------------------'
+  if(root) print *,' -t- , -dt- '
+  do while(flow%time*f/c<15 .and..not.there)
+    call update_line()
+    if(flowing) then
+      call geom%update(flow%time+flow%dt) ! just sets a flag
+      call flow%update(geom)
+      flow%dt = min(flow%dt,0.5)
+      write(9,'(f10.4,f8.4,8e16.8)') flow%time*f/c,flow%dt, &
+         2./Re*geom%vforce(flow%velocity), &
+        -2./c*geom%pforce(flow%pressure), &
+         2./Re*geom%vpower(flow%velocity), &
+        -2./c*geom%ppower(flow%pressure)
+      flush(9)
+      if(mod(flow%time,0.125*c/f)<flow%dt) then
+        if(root) print '(f7.3,",",f6.3)',flow%time*f/c,flow%dt
+        call display(flow%velocity%vorticity_Z(), 'out_vort', lim = 0.25, box=box)
+      end if
+    else
+      flow%time = flow%time+flow%dt
+      if(mod(flow%time,c/f)<flow%dt) print *,flow%time*f/c
+    end if
+    inquire(file='.kill', exist=there)
+  end do
+  if(root) print *,'Loop complete: writing restart files and exiting'
+  if(root) print *,'-----------------------------------'
+  call flow%write()
+  call mympi_end
+contains
+  type(set) function rect() result(geom)
+    geom = plane(norm=(/-1,0,0/),center=(/0,-2,0/)) &
+         .and.plane(norm=(/0,-1,0/),center=(/0,-2,0/)) &
+         .and.plane(norm=(/1,0,0/),center=(/c,2.,0./)) &
+         .and.plane(norm=(/0,1,0/),center=(/c,2.,0./))
+  end function
+!
+! -- free centerline motion
+  subroutine update_line
+    real,allocatable :: fp(:,:)
+    real :: yn(s),q(s),resid,y_w
+    integer :: i,j
+
+    ! get stress and leading edge position
+    if(flowing) then
+    fp = -geom%pforce_plane(flow%pressure)/h_min
+      do i=1,s
+        j = xg(1)%hash(int(h_min*(i-0.5)))
+        q(i) = fp(j,1)+mu_a*ddoty(i)
+      end do
+    else
+      q = 0
+    end if
+    y_w = 5*sin(-2*pi*f*(flow%time+flow%dt)/c)
+
+    !! solve for the new y array with SOR
+    yn = y
+    do i=1,100
+      resid=sweep(y_w,q,yn)
+      if(resid<1e-6) exit
+    end do
+    if(resid>1e-4) print *,'structural residual:',resid
+    ddoty = (2*yn(i)+5*y(i)-4*y0(i)+y00(i))/flow%dt**2
+    doty = (3*yn-4*y+y0)/(2.*flow%dt)
+    y00 = y0; y0 = y; y = yn
+
+    !! print
+    if(mod(flow%time+flow%dt,0.125*c/f)<flow%dt) then
+      write(10,1) (flow%time*f/c,h_min/c*(i-0.5),q(i),y(i),i=1,s)
+1     format(4e12.4)
+      flush(10)
+      stop
+    end if
+  end subroutine update_line
+
+  real function sweep(y_w,q,yn)
+    real,intent(in) :: y_w,q(s)
+    real,intent(inout) :: yn(s)
+    real :: d4y,aii,r,omega=1.5
+    integer :: i
+    sweep = 0
+    do i=1,s
+      if(i==1) then !clamped
+        d4y = 1.92*yn(i+2)-10.67*yn(i+1)+48.*yn(i)-39.25*y_w
+        aii = 48.0
+      else if(i==2) then !clamped
+        d4y = yn(i+2)-3.96*yn(i+1)+5.67*yn(i)-yn(i-1)-1.71*y_w
+        aii = 5.67
+      ! if(i==1) then !pinned
+      !   d4y = 1.67*yn(i+2)-8.35*yn(i+1)+16.70*yn(i)-10.02*y_w
+      !   aii = 16.70
+      ! else if(i==2) then !pinned
+      !   d4y = yn(i+2)-3.991*yn(i+1)+5.956*yn(i)-4.913*yn(i-1)+1.948*y_w
+      !   aii = 5.956
+      else if(i==s-1) then ! free
+        d4y = -1.828*yn(i+1)+4.656*yn(i)-3.828*yn(i-1)+yn(i-2)
+        aii = 4.656
+      else if(i==s) then ! free
+        d4y = 0.828*yn(i-2)-1.656*yn(i-1)+0.828*yn(i)
+        aii = 0.828
+      else
+        d4y = yn(i+2)-4*yn(i+1)+6*yn(i)-4*yn(i-1)+yn(i-2)
+        aii = 6
+      end if
+      aii = 2+flow%dt**2*EI*aii/h_min**4/(mu+mu_a)
+      r=-2*yn(i)+5*y(i)-4*y0(i)+y00(i)+flow%dt**2*(q(i)-EI*d4y/h_min**4)/(mu+mu_a)-0.01*flow%dt**2*doty(i)
+      yn(i) = yn(i)+omega*r/aii
+      sweep = sweep+r**2
+    end do
+    sweep = sqrt(sweep/s)
+  end function sweep
+
+  real pure function h(x)
+    real,intent(in) :: x(3)
+    h = interp(y,x(1))
+  end function h
+  real pure function doth(x)
+    real,intent(in) :: x(3)
+    doth = interp(doty,x(1))
+  end function doth
+  pure function dh(x)
+    real,intent(in) :: x(3)
+    real            :: dh(3)
+    dh = 0
+    dh(1) = interp(y,x(1)+0.5)-interp(y,x(1)-0.5)
+  end function dh
+  real pure function interp(y,x)
+    real,intent(in) :: y(:),x
+    integer :: i
+    real :: r
+    if(x>c+4 .or. x<-4) then
+      interp = 0
+    else
+      i = floor(x/h_min+0.5)
+      i = min(max(1,i),s-1)
+      r = x/h_min-(i-0.5)
+      interp = y(i)*(1-r)+y(i+1)*r
+    end if
+  end function interp
+end program fish
---
+++ flag/postproc/stat.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# ----------------------------------------- #
+# stat.py
+# ----------------------------------------- #
+#
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+#
+# -- read data
+try:
+    df = pd.read_csv('fort.9',delim_whitespace = True,
+        names=["time","CFL","fx","fy","fz","px","py","pz","fp","pp"])
+except FileNotFoundError:
+    exit('stat: fort.9 not found')
+try:
+    mg = pd.read_csv('fort.8',delim_whitespace = True,
+        names=["itr","res0","res","inf"])[2:]
+except FileNotFoundError:
+    exit('stat: fort.8 not found')
+try:
+    ln = pd.read_csv('fort.10',delim_whitespace = True,
+        names=["time","x","doty","y"])
+except FileNotFoundError:
+    exit('stat: fort.10 not found')
+#
+# -- post process signals
+df['x'] = df['fx']+df['px']
+df['y'] = df['fy']+df['py']
+df['p'] = df['fp']+df['pp']
+df.query('time > 0.1', inplace=True)
+late = df.query('time > 5')
+
+ln['cycle'] = np.floor(ln.time)
+ln['phase'] = np.rint((ln.time-ln.cycle)*8)
+ln.query('time > 7', inplace=True)
+averaged = (ln.groupby(['phase','x'], as_index=False).mean() # take mean for each phase and x
+            .groupby('phase')) # regroup by phase
+last = ln.query('time > 14').groupby('phase')
+#
+# -- plot PDF pages
+def plot_hist(pdf,name,label):
+    ax = df.plot(x='time',y=name,figsize=(8,4))
+    plt.xlabel(r'$t/T$', fontsize=12)
+    plt.ylabel(label, fontsize=12)
+    txt = 'mean='+str(round(late[name].mean(),4)) \
+        +', mad='+str(round(1.5748*late[name].mad(),4))
+    plt.text(0.5,0.01,txt,transform=ax.transAxes)
+    pdf.savefig()
+
+with PdfPages('history.pdf') as pdf:
+    plot_hist(pdf,name='x',label=r'$C_D$')
+    plot_hist(pdf,name='y',label=r'$C_L$')
+    plot_hist(pdf,name='p',label=r'$C_P$')
+    # plot_hist(pdf,name='fx',label=r'$C_{Df}$')
+    # plot_hist(pdf,name='fy',label=r'$C_{Lf}$')
+    # plot_hist(pdf,name='fp',label=r'$C_{Pf}$')
+    # plot_hist(pdf,name='px',label=r'$C_{Dp}$')
+    # plot_hist(pdf,name='py',label=r'$C_{Lp}$')
+    # plot_hist(pdf,name='pp',label=r'$C_{Pp}$')
+    # plot_hist(pdf,name='CFL',label='CFL')
+
+    fig, ax = plt.subplots(figsize=(8,4))
+    for name, group in averaged:
+        arg = 2.*np.pi*name/8.
+        cols = (1/2-np.cos(arg)/2,0.125,1/2-np.sin(arg)/2)
+        group.plot(x='x',y='y',ax=ax,label=r'$\phi=$'+'{}/8'.format(int(name)),c=cols)
+    plt.xlabel(r'$x/c$', fontsize=12)
+    plt.ylabel(r'$\widetilde y/c$', fontsize=12)
+    ax.legend(ncol=2)
+    pdf.savefig()
+
+    fig, ax = plt.subplots(figsize=(8,4))
+    for name, group in averaged:
+        arg = 2.*np.pi*name/8.
+        cols = (1/2-np.cos(arg)/2,0.125,1/2-np.sin(arg)/2)
+        group.plot(x='x',y='doty',ax=ax,label=r'$\phi=$'+'{}/8'.format(int(name)),c=cols)
+    plt.xlabel(r'$x/c$', fontsize=12)
+    plt.ylabel(r'$\widetilde q$', fontsize=12)
+    ax.legend(ncol=2)
+    pdf.savefig()
+
+    fig, ax = plt.subplots(figsize=(8,4))
+    for name, group in last:
+        arg = 2.*np.pi*name/8.
+        cols = (1/2-np.cos(arg)/2,0.125,1/2-np.sin(arg)/2)
+        group.plot(x='x',y='y',ax=ax,label=r'$\phi=$'+'{}/8'.format(int(name)),c=cols)
+    plt.xlabel(r'$x/c$', fontsize=12)
+    plt.ylabel(r'$y/c$', fontsize=12)
+    ax.legend(ncol=2)
+    pdf.savefig()
+
+    fig, ax = plt.subplots(figsize=(8,4))
+    for name, group in last:
+        arg = 2.*np.pi*name/8.
+        cols = (1/2-np.cos(arg)/2,0.125,1/2-np.sin(arg)/2)
+        group.plot(x='x',y='doty',ax=ax,label=r'$\phi=$'+'{}/8'.format(int(name)),c=cols)
+    plt.xlabel(r'$x/c$', fontsize=12)
+    plt.ylabel(r'$q$', fontsize=12)
+    ax.legend(ncol=2)
+    pdf.savefig()
+
+    mg.plot(y=['res0','res','inf'],figsize=(8,4))
+    plt.yscale('log')
+    pdf.savefig()
+
+    mg.plot(y='itr',figsize=(8,4))
+    pdf.savefig()
