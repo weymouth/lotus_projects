235ee95d79c42faa6e2f45aa29cafe6b3496a9ad
Gabriel Weymouth <drweymouth@google.com>
2018-07-15 09:33:18 +0100

    working on pulse efficiency

--- pulse/compare.py
+++ pulse/compare.py
@@ -11,9 +11,10 @@ import matplotlib.pyplot as plt
 def average(df,var):
     return trapz(df[var],df.time)/(df.time.iloc[-1]-df.time.iloc[0])
 
+def cycle_mean(df,var):
+    return [average(group,var) for cycle,group in df.groupby('cycle')]
 def print_means(df,var):
-    l = [average(group,var) for cycle,group in df.groupby('cycle')]
-    print(var,', '.join('{:.3g}'.format(i) for i in l))
+    print(var,', '.join('{:.3g}'.format(i) for i in cycle_mean(df,var)))
 
 def read_it(folder,trim=True):
     try:
@@ -26,7 +27,20 @@ def read_it(folder,trim=True):
         exit('stat: '+folder+'/fort.9 not found')
     return df
 
-for folder in ['dm10_U100','dm10_free','dm40_U100','dm40_free']:
-    print(folder)
-    df = read_it(folder)
-    [print_means(df,var) for var in ['drag','power','U']]
+# df = read_it('AoAe4_dm20/free',trim=False)
+# df[["drag","lift","side","power"]] *= 4/1.25 # correct the area-scale
+# df[["time","CFL","drag","lift","side","power","U"]].to_csv(r'dm20_free/fort.9', header=None, index=None, sep=' ', mode='a')
+
+R100 = cycle_mean(read_it('towU100'),'drag')[-1]
+R112 = cycle_mean(read_it('towU112'),'drag')[-1]/(1.12)**2
+def RU(U):
+    r = (U-1)/(0.12)
+    return (R100*(1-r)+R112*r)*U**3
+
+# for dm in [5,10,15,20,25,30,40,50,60]:
+#     for folder in ['dm{:02d}_'.format(dm)+s for s in ['U100','free']]:
+#         print(folder)
+#         df = read_it(folder)
+#         [print_means(df,var) for var in ['drag','power','U']]
+
+dm = [5,10,15,20,25,30,40]
