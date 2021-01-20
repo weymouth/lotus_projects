5816411a5b5686cb12c71fe04bf001ed9a67a282
Gabriel Weymouth <drweymouth@google.com>
2021-01-20 09:25:26 +0000

    Update lotus.f90
    This commit adds a check for streched grids. Confirms identical force calculation as long as the integration region is within the uniform grid section.

--- force_check/lotus.f90
+++ force_check/lotus.f90
@@ -1,16 +1,21 @@
 program force_check
   use geom_shape, only: sphere,operator(.map.),init_rigid
+  use gridMod,    only: xg
   use bodyMod,    only: body
   use fluidMod,   only: fluid
   implicit none
 !
 ! -- Define parameters, declare variables
   real,parameter    :: D = 64 ! diameter
-  integer           :: n(3) = D*(/2,2,2/),time
+  integer           :: n(3) = D*(/2,2,2/),time,i
   type(fluid) :: flow
   type(body)  :: geom
 !
-! -- Initialize body and fluid
+! -- Initialize
+  xg%left = -D  ! uniform grid
+  ! do i=1,3 ! stretched grid
+  !   call xg(i)%stretch(n(i),-2*D,-0.5*D-6,0.5*D+6,2*D,prnt=.true.)
+  ! end do
   geom = sphere(radius=D/2, center=0) &  ! sphere
           .map.init_rigid(1,x).map.init_rigid(2,y).map.init_rigid(3,z)
 
@@ -29,15 +34,15 @@ contains
 
   real(8) pure function x(t)
     real(8),intent(in) :: t
-    x = D+2*sin(t/3)
+    x = 2*sin(t/3)
   end function x
   real(8) pure function y(t)
     real(8),intent(in) :: t
-    y = D+2*sin(t/5)
+    y = 2*sin(t/5)
   end function y
   real(8) pure function z(t)
     real(8),intent(in) :: t
-    z = D+2*cos(t/7)
+    z = 2*cos(t/7)
   end function z
   real pure function linear(x)
     real,intent(in) :: x(3)
